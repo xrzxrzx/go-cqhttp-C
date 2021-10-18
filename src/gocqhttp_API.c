975f0693186929c1600ed47f7860ebb1ec5df003
jingyuexing <MayorTalor@163.com>
2021-10-19 02:45:13 +0800

    * format file * fixed `CMakeLists.txt` file

--- src/gocqhttp_API.c
+++ src/gocqhttp_API.c
@@ -1,239 +1,215 @@
-﻿#include"gocqhttp_API.h"
-#include<process.h>
-#include<winsock2.h>
-#include<windows.h>
-#include<WS2tcpip.h>
-#include<stdio.h>
-#include<string.h>
+#include "gocqhttp_API.h"
+#include <WS2tcpip.h>
+#include <process.h>
+#include <stdio.h>
+#include <string.h>
+#include <windows.h>
+#include <winsock2.h>
 #pragma comment(lib, "ws2_32.lib")
 
 /* socket */
-static SOCKADDR_IN	server_addr;//服务端
-static SOCKET		server;		//新建套接字
-
+static SOCKADDR_IN server_addr; //服务端
+static SOCKET server;           //新建套接字
 
 /*send_private_msg 发送私密消息*/
-send_private_msg_data* New_send_private_msg(unsigned long user_id, unsigned long group_id, char message[1500], int auto_escape)
-{
-	send_private_msg_data* data = (send_private_msg_data*)malloc(sizeof(send_private_msg_data));
-	if (!data)
-		return NULL;
-	data->send_msg.user_id = user_id;
-	data->send_msg.group_id = group_id;
-	strcpy(data->send_msg.message, message);
-	data->send_msg.auto_escape = auto_escape;
-	return data;
+send_private_msg_data *New_send_private_msg(unsigned long user_id,
+                                            unsigned long group_id,
+                                            char message[1500],
+                                            int auto_escape) {
+  send_private_msg_data *data =
+      (send_private_msg_data *)malloc(sizeof(send_private_msg_data));
+  if (!data)
+    return NULL;
+  data->send_msg.user_id = user_id;
+  data->send_msg.group_id = group_id;
+  strcpy(data->send_msg.message, message);
+  data->send_msg.auto_escape = auto_escape;
+  return data;
 }
 
-cqhttp_err send_private_msg(send_private_msg_data* data)
-{
-	char func[70] = "send_private_msg";
+cqhttp_err send_private_msg(send_private_msg_data *data) {
+  char func[70] = "send_private_msg";
 
-	if ((server = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		return set_cqhttp_err(SocketInitError, func, 0, NULL);
-	if (connect(server, (SOCKADDR*)&server_addr, sizeof(server_addr)) < 0)
-		return set_cqhttp_err(ConnectionError, func, 0, NULL);
+  if ((server = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    return set_cqhttp_err(SocketInitError, func, 0, NULL);
+  if (connect(server, (SOCKADDR *)&server_addr, sizeof(server_addr)) < 0)
+    return set_cqhttp_err(ConnectionError, func, 0, NULL);
 
-	char rmsg[1024] = { '\0' };							//收包
-	char smsg[1024] = { '\0' };							//发包
-	memset(smsg, 0, 1024);
+  char rmsg[1024] = {'\0'}; //收包
+  char smsg[1024] = {'\0'}; //发包
+  memset(smsg, 0, 1024);
 
-	//构建发包（字符串）
-	sprintf(smsg, API_SEND_PRIVATE_MSG_FORM,
-			data->send_msg.user_id,
-			data->send_msg.group_id,
-			data->send_msg.message,
-			data->send_msg.auto_escape);
-	int isend = send(server, smsg, strlen(smsg), 0);	//发送
-	if (isend < 0)
-	{
-		closesocket(server);
-		return set_cqhttp_err(NetworkIOError, func, 1, "发包发送失败");
-	}
+  //构建发包（字符串）
+  sprintf(smsg, API_SEND_PRIVATE_MSG_FORM, data->send_msg.user_id,
+          data->send_msg.group_id, data->send_msg.message,
+          data->send_msg.auto_escape);
+  int isend = send(server, smsg, strlen(smsg), 0); //发送
+  if (isend < 0) {
+    closesocket(server);
+    return set_cqhttp_err(NetworkIOError, func, 1, "发包发送失败");
+  }
 
-	memset(data, 0, sizeof(send_private_msg_data));
-	while (recv(server, rmsg, sizeof(rmsg), 0) < 0);	//接收
-	if(sscanf(rmsg, API_SEND_PRIVATE_MSG_RECV,
-				&data->recv_msg.data.message_id,
-				&data->recv_msg.retcode,
-				data->recv_msg.status) == -1)
-		cqhttp_err_out(set_cqhttp_err(StringError, func, 1, "sscanf"));
+  memset(data, 0, sizeof(send_private_msg_data));
+  while (recv(server, rmsg, sizeof(rmsg), 0) < 0)
+    ; //接收
+  if (sscanf(rmsg, API_SEND_PRIVATE_MSG_RECV, &data->recv_msg.data.message_id,
+             &data->recv_msg.retcode, data->recv_msg.status) == -1)
+    cqhttp_err_out(set_cqhttp_err(StringError, func, 1, "sscanf"));
 
-	closesocket(server);
-	return set_cqhttp_err(None, func, 0, NULL);
+  closesocket(server);
+  return set_cqhttp_err(None, func, 0, NULL);
 }
 
 /*send_group_msg 发送群消息*/
-send_group_msg_data* New_send_group_msg(unsigned long group_id, char message[1500], int auto_escape)
-{
-	send_group_msg_data* data = (send_group_msg_data*)malloc(sizeof(send_group_msg_data));
-	if (!data)
-		return NULL;
-	data->send_msg.group_id = group_id;
-	strcpy(data->send_msg.message, message);
-	data->send_msg.auto_escape = auto_escape;
-	return data;
+send_group_msg_data *New_send_group_msg(unsigned long group_id,
+                                        char message[1500], int auto_escape) {
+  send_group_msg_data *data =
+      (send_group_msg_data *)malloc(sizeof(send_group_msg_data));
+  if (!data)
+    return NULL;
+  data->send_msg.group_id = group_id;
+  strcpy(data->send_msg.message, message);
+  data->send_msg.auto_escape = auto_escape;
+  return data;
 }
 
-cqhttp_err send_group_msg(send_group_msg_data* data)
-{
-	char func[70] = "send_group_msg";
+cqhttp_err send_group_msg(send_group_msg_data *data) {
+  char func[70] = "send_group_msg";
 
-	if ((server = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		return set_cqhttp_err(SocketInitError, func, 0, NULL);
-	if (connect(server, (SOCKADDR*)&server_addr, sizeof(server_addr)) < 0)
-		return set_cqhttp_err(ConnectionError, func, 0, NULL);
+  if ((server = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    return set_cqhttp_err(SocketInitError, func, 0, NULL);
+  if (connect(server, (SOCKADDR *)&server_addr, sizeof(server_addr)) < 0)
+    return set_cqhttp_err(ConnectionError, func, 0, NULL);
 
-	char rmsg[1024] = { '\0' };			//收包
-	char smsg[1024] = { '\0' };			//发包
-	memset(smsg, 0, 1024);
+  char rmsg[1024] = {'\0'}; //收包
+  char smsg[1024] = {'\0'}; //发包
+  memset(smsg, 0, 1024);
 
-	//构建发包
-	sprintf(smsg, API_SEND_GROUP_MSG_FORM,
-			data->send_msg.group_id,
-			data->send_msg.message,
-			data->send_msg.auto_escape);
-	int isend = send(server, smsg, strlen(smsg), 0);	//发送
-	if (isend < 0)
-	{
-		closesocket(server);
-		return set_cqhttp_err(NetworkIOError, func, 1, "发包发送失败");
-	}
+  //构建发包
+  sprintf(smsg, API_SEND_GROUP_MSG_FORM, data->send_msg.group_id,
+          data->send_msg.message, data->send_msg.auto_escape);
+  int isend = send(server, smsg, strlen(smsg), 0); //发送
+  if (isend < 0) {
+    closesocket(server);
+    return set_cqhttp_err(NetworkIOError, func, 1, "发包发送失败");
+  }
 
-	memset(data, 0, sizeof(send_group_msg_data));
-	while (recv(server, rmsg, sizeof(rmsg), 0) < 0);	//接收
-	if (sscanf(rmsg, API_SEND_GROUP_MSG_RECV,
-		&data->recv_msg.data.message_id,
-		&data->recv_msg.retcode,
-		data->recv_msg.status) == -1)
-		cqhttp_err_out(set_cqhttp_err(StringError, func, 1, "sscanf"));
+  memset(data, 0, sizeof(send_group_msg_data));
+  while (recv(server, rmsg, sizeof(rmsg), 0) < 0)
+    ; //接收
+  if (sscanf(rmsg, API_SEND_GROUP_MSG_RECV, &data->recv_msg.data.message_id,
+             &data->recv_msg.retcode, data->recv_msg.status) == -1)
+    cqhttp_err_out(set_cqhttp_err(StringError, func, 1, "sscanf"));
 
-	closesocket(server);
-	return set_cqhttp_err(None, func, 0, NULL);
+  closesocket(server);
+  return set_cqhttp_err(None, func, 0, NULL);
 }
 
 /*get_msg 获取消息*/
-get_msg_data* New_get_msg(int message_id)
-{
-	get_msg_data* data = (get_msg_data*)malloc(sizeof(get_msg_data));
-	if (!data)
-		return NULL;
-	data->send_msg.message_id = message_id;
-	return data;
+get_msg_data *New_get_msg(int message_id) {
+  get_msg_data *data = (get_msg_data *)malloc(sizeof(get_msg_data));
+  if (!data)
+    return NULL;
+  data->send_msg.message_id = message_id;
+  return data;
 }
 
-cqhttp_err get_msg(get_msg_data* data)
-{
-	char func[70] = "get_msg";
+cqhttp_err get_msg(get_msg_data *data) {
+  char func[70] = "get_msg";
 
-	if ((server = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		return set_cqhttp_err(SocketInitError, func, 0, NULL);
-	if (connect(server, (SOCKADDR*)&server_addr, sizeof(server_addr)) < 0)
-		return set_cqhttp_err(ConnectionError, func, 0, NULL);
+  if ((server = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    return set_cqhttp_err(SocketInitError, func, 0, NULL);
+  if (connect(server, (SOCKADDR *)&server_addr, sizeof(server_addr)) < 0)
+    return set_cqhttp_err(ConnectionError, func, 0, NULL);
 
-	char rmsg[1024] = { '\0' };							//收包
-	char smsg[1024] = { '\0' };							//发包
-	memset(smsg, 0, 1024);
+  char rmsg[1024] = {'\0'}; //收包
+  char smsg[1024] = {'\0'}; //发包
+  memset(smsg, 0, 1024);
 
-	//构建发包
-	sprintf(smsg, API_GET_MSG_FORM,
-		data->send_msg.message_id);
-	int isend = send(server, smsg, strlen(smsg), 0);	//发送
-	if (isend < 0)
-	{
-		closesocket(server);
-		return set_cqhttp_err(NetworkIOError, func, 1, "发包发送失败");
-	}
+  //构建发包
+  sprintf(smsg, API_GET_MSG_FORM, data->send_msg.message_id);
+  int isend = send(server, smsg, strlen(smsg), 0); //发送
+  if (isend < 0) {
+    closesocket(server);
+    return set_cqhttp_err(NetworkIOError, func, 1, "发包发送失败");
+  }
 
-	memset(data, 0, sizeof(delete_msg_data));
-	while (recv(server, rmsg, sizeof(rmsg), 0) < 0);	//接收
-	if (sscanf(rmsg, API_GET_MSG_RECV,
-		data->recv_msg.data.group,
-		&data->recv_msg.data.group_id,
-		data->recv_msg.data.message,
-		&data->recv_msg.data.message_id,
-		&data->recv_msg.data.message_seq,
-		data->recv_msg.data.message_type,
-		data->recv_msg.data.raw_message,
-		&data->recv_msg.data.real_id,
-		data->recv_msg.data.sender.nickname,
-		&data->recv_msg.data.sender.user_id,
-		&data->recv_msg.data.time,
-		&data->recv_msg.retcode,
-		data->recv_msg.status) == -1)
-		cqhttp_err_out(set_cqhttp_err(StringError, func, 1, "sscanf"));
+  memset(data, 0, sizeof(delete_msg_data));
+  while (recv(server, rmsg, sizeof(rmsg), 0) < 0)
+    ; //接收
+  if (sscanf(rmsg, API_GET_MSG_RECV, data->recv_msg.data.group,
+             &data->recv_msg.data.group_id, data->recv_msg.data.message,
+             &data->recv_msg.data.message_id, &data->recv_msg.data.message_seq,
+             data->recv_msg.data.message_type, data->recv_msg.data.raw_message,
+             &data->recv_msg.data.real_id, data->recv_msg.data.sender.nickname,
+             &data->recv_msg.data.sender.user_id, &data->recv_msg.data.time,
+             &data->recv_msg.retcode, data->recv_msg.status) == -1)
+    cqhttp_err_out(set_cqhttp_err(StringError, func, 1, "sscanf"));
 
-	closesocket(server);
-	return set_cqhttp_err(None, func, 0, NULL);
+  closesocket(server);
+  return set_cqhttp_err(None, func, 0, NULL);
 }
 
 /*delete_msg 撤回消息*/
-delete_msg_data* New_delete_msg(int message_id)
-{
-	delete_msg_data* data = (delete_msg_data*)malloc(sizeof(delete_msg_data));
-	if (!data)
-		return NULL;
-	data->send_msg.message_id = message_id;
-	return data;
+delete_msg_data *New_delete_msg(int message_id) {
+  delete_msg_data *data = (delete_msg_data *)malloc(sizeof(delete_msg_data));
+  if (!data)
+    return NULL;
+  data->send_msg.message_id = message_id;
+  return data;
 }
 
-cqhttp_err delete_msg(delete_msg_data* data)
-{
-	char func[70] = "delete_msg";
+cqhttp_err delete_msg(delete_msg_data *data) {
+  char func[70] = "delete_msg";
 
-	if ((server = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-		return set_cqhttp_err(SocketInitError, func, 0, NULL);
-	if (connect(server, (SOCKADDR*)&server_addr, sizeof(server_addr)) < 0)
-		return set_cqhttp_err(ConnectionError, func, 0, NULL);
+  if ((server = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    return set_cqhttp_err(SocketInitError, func, 0, NULL);
+  if (connect(server, (SOCKADDR *)&server_addr, sizeof(server_addr)) < 0)
+    return set_cqhttp_err(ConnectionError, func, 0, NULL);
 
-	char rmsg[1024] = { '\0' };							//收包
-	char smsg[1024] = { '\0' };							//发包
-	memset(smsg, 0, 1024);
+  char rmsg[1024] = {'\0'}; //收包
+  char smsg[1024] = {'\0'}; //发包
+  memset(smsg, 0, 1024);
 
-	//构建发包
-	sprintf(smsg, API_DELETE_MSG_FORM,
-		data->send_msg.message_id);
-	int isend = send(server, smsg, strlen(smsg), 0);	//发送
-	if (isend < 0)
-	{
-		closesocket(server);
-		return set_cqhttp_err(NetworkIOError, func, 1, "发包发送失败");
-	}
+  //构建发包
+  sprintf(smsg, API_DELETE_MSG_FORM, data->send_msg.message_id);
+  int isend = send(server, smsg, strlen(smsg), 0); //发送
+  if (isend < 0) {
+    closesocket(server);
+    return set_cqhttp_err(NetworkIOError, func, 1, "发包发送失败");
+  }
 
-	memset(data, 0, sizeof(data));
-	while (recv(server, rmsg, sizeof(rmsg), 0) < 0);	//接收
-	if (sscanf(rmsg, API_DELETE_MSG_RECV,
-		data->recv_msg.data,
-		&data->recv_msg.retcode,
-		data->recv_msg.status) == -1)
-		cqhttp_err_out(set_cqhttp_err(StringError, func, 1, "sscanf"));
+  memset(data, 0, sizeof(data));
+  while (recv(server, rmsg, sizeof(rmsg), 0) < 0)
+    ; //接收
+  if (sscanf(rmsg, API_DELETE_MSG_RECV, data->recv_msg.data,
+             &data->recv_msg.retcode, data->recv_msg.status) == -1)
+    cqhttp_err_out(set_cqhttp_err(StringError, func, 1, "sscanf"));
 
-	closesocket(server);
-	return set_cqhttp_err(None, func, 0, NULL);
+  closesocket(server);
+  return set_cqhttp_err(None, func, 0, NULL);
 }
 
 /*初始化*/
-cqhttp_err init_gocqhttpAPI(const char* ip, const int port)
-{
-	const char func[70] = "init_gocqhttpAPI";
+cqhttp_err init_gocqhttpAPI(const char *ip, const int port) {
+  const char func[70] = "init_gocqhttpAPI";
 
-	memset((void*)&server_addr, 0, sizeof(SOCKADDR_IN));
-	
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(port);
-	inet_pton(AF_INET, ip, (void*)&server_addr.sin_addr);
+  memset((void *)&server_addr, 0, sizeof(SOCKADDR_IN));
 
-	WSADATA wsaData;
-	int e = WSAStartup(MAKEWORD(2, 2), &wsaData);
-	if (e)
-		return set_cqhttp_err(WSAStartupError, func, 1, "初始化动态库失败");
-	
-	return set_cqhttp_err(None, func, 0, NULL);
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(port);
+  inet_pton(AF_INET, ip, (void *)&server_addr.sin_addr);
+
+  WSADATA wsaData;
+  int e = WSAStartup(MAKEWORD(2, 2), &wsaData);
+  if (e)
+    return set_cqhttp_err(WSAStartupError, func, 1, "初始化动态库失败");
+
+  return set_cqhttp_err(None, func, 0, NULL);
 }
 
 /*退出*/
-void exit_gocqhttpAPI(void)
-{
-	closesocket(server);  
-	memset((void*)&server_addr, 0, sizeof(server_addr));
+void exit_gocqhttpAPI(void) {
+  closesocket(server);
+  memset((void *)&server_addr, 0, sizeof(server_addr));
 }
--- src/main.c
+++ src/main.c
@@ -1,105 +1,99 @@
-﻿#include"URLcode.h"
-#include"gocqhttp_API.h"
-#include"gocqhttp_Event.h"
-#include"gocqhttp_err.h"
-#include"robot.h"
-#include<stdio.h>
-#include<stdlib.h>
-#include<winsock2.h>
-#include<windows.h>
-#include<WS2tcpip.h>
-#include<string.h>
-#include<errno.h>
-#include<time.h>
+#include "URLcode.h"
+#include "gocqhttp_API.h"
+#include "gocqhttp_Event.h"
+#include "gocqhttp_err.h"
+#include "robot.h"
+#include <WS2tcpip.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <windows.h>
+#include <winsock2.h>
 #pragma comment(lib, "ws2_32.lib")
 
 const char ip[] = "127.0.0.1";
 const int port_API = 5700;
 const int port_Event = 5701;
 
-void event_switch(void* data);		//事件检索
+void event_switch(void *data); //事件检索
 
-int main(void)
-{
-	cqhttp_err err;
+int main(void) {
+  cqhttp_err err;
 
-	RESTART:
-	err = init_gocqhttpAPI(ip, port_API);
-	cqhttp_err_out(err);
-	err = init_gocqhttpEvent(ip, port_Event, event_switch);
-	if (err.error != None)
-	{
-		puts("5秒后开始重连");
-		Sleep(5000);
-		goto RESTART;
-	}
-	cqhttp_err_out(err);
-	err = recv_event();
-	cqhttp_err_out(err);
-	if (err.error == ConnectionError)
-	{
-		puts("5秒后开始重连");
-		Sleep(5000);
-		goto RESTART;
-	}
+RESTART:
+  err = init_gocqhttpAPI(ip, port_API);
+  cqhttp_err_out(err);
+  err = init_gocqhttpEvent(ip, port_Event, event_switch);
+  if (err.error != None) {
+    puts("5秒后开始重连");
+    Sleep(5000);
+    goto RESTART;
+  }
+  cqhttp_err_out(err);
+  err = recv_event();
+  cqhttp_err_out(err);
+  if (err.error == ConnectionError) {
+    puts("5秒后开始重连");
+    Sleep(5000);
+    goto RESTART;
+  }
 
-	return 0;
+  return 0;
 }
 
 /*事件检索*/
-void event_switch(void* data)
-{
-	char func[70] = "event_switch";
+void event_switch(void *data) {
+  char func[70] = "event_switch";
 
-	char* event_msg = (char*)data;	//待处理的事件
-	event_data msg;					//已解析事件
-	cqhttp_err err;
-	
-	switch (event_type_switch(event_msg))
-	{
-		case message_event:		//消息事件
-			cqhttp_err_out(set_cqhttp_err(None, func, 1, "recv message_event"));
-			switch (message_type_switch(event_msg))
-			{
-				case private_message:	//私聊消息
-					cqhttp_err_out(set_cqhttp_err(None, func, 1, "recv parivate_message"));
-					msg.private_message = private_message_event_analysis(event_msg);
-					//code
-					break;
-				case group_message:		//群消息
-					cqhttp_err_out(set_cqhttp_err(None, func, 1, "recv group_message"));
-					msg.group_message = group_message_event_analysis(event_msg);、
-					//code
-					break;
-				case unknow_message:	//未知
-					cqhttp_err_out(set_cqhttp_err(None, func, 1, "recv unknow_message"));
-					//code
-					break;
-			}
-			break;
-		case notice_event:		//通知事件
-			cqhttp_err_out(set_cqhttp_err(None, func, 1, "recv notice_event"));
-			switch (notice_type_switch(event_msg))
-			{
-				case friend_recall_notice:
-					cqhttp_err_out(set_cqhttp_err(None, func, 1, "recv friend_recall_notice"));
-					//code
-					break;
-				case group_recall_notice:
-					cqhttp_err_out(set_cqhttp_err(None, func, 1, "recv group_recall_notice"));
-					msg.group_recall = group_recall_notice_analysis(event_msg);
-					//code
-					break;
-			}
-			break;
-		case meta_event:		//心跳事件
-			cqhttp_err_out(set_cqhttp_err(None, func, 1, "recv meta_message"));
-			//code
-			break;
-		case unknow_event:		//未知事件
-			cqhttp_err_out(set_cqhttp_err(None, func, 1, "recv unknow_event"));
-			//code
-			break;
-	}
-	free(event_msg);
+  char *event_msg = (char *)data; //待处理的事件
+  event_data msg;                 //已解析事件
+  cqhttp_err err;
+
+  switch (event_type_switch(event_msg)) {
+  case message_event: //消息事件
+    cqhttp_err_out(set_cqhttp_err(None, func, 1, "recv message_event"));
+    switch (message_type_switch(event_msg)) {
+    case private_message: //私聊消息
+      cqhttp_err_out(set_cqhttp_err(None, func, 1, "recv parivate_message"));
+      msg.private_message = private_message_event_analysis(event_msg);
+      // code
+      break;
+    case group_message: //群消息
+      cqhttp_err_out(set_cqhttp_err(None, func, 1, "recv group_message"));
+      msg.group_message = group_message_event_analysis(event_msg);
+      // code
+      break;
+    case unknow_message: //未知
+      cqhttp_err_out(set_cqhttp_err(None, func, 1, "recv unknow_message"));
+      // code
+      break;
+    }
+    break;
+  case notice_event: //通知事件
+    cqhttp_err_out(set_cqhttp_err(None, func, 1, "recv notice_event"));
+    switch (notice_type_switch(event_msg)) {
+    case friend_recall_notice:
+      cqhttp_err_out(
+          set_cqhttp_err(None, func, 1, "recv friend_recall_notice"));
+      // code
+      break;
+    case group_recall_notice:
+      cqhttp_err_out(set_cqhttp_err(None, func, 1, "recv group_recall_notice"));
+      msg.group_recall = group_recall_notice_analysis(event_msg);
+      // code
+      break;
+    }
+    break;
+  case meta_event: //心跳事件
+    cqhttp_err_out(set_cqhttp_err(None, func, 1, "recv meta_message"));
+    // code
+    break;
+  case unknow_event: //未知事件
+    cqhttp_err_out(set_cqhttp_err(None, func, 1, "recv unknow_event"));
+    // code
+    break;
+  }
+  free(event_msg);
 }
